22eb6f368a71c4c8ade1b28eeb96b56020c0451b
sujinjeongg <102423490+sujinjeongg@users.noreply.github.com>
2025-03-19 15:54:35 +0900

    [level 3] Title: 봉인된 주문, Time: 1809.49 ms, Memory: 50.3 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
@@ -1,10 +1,12 @@
 def solution(n, bans):
     def spell2num(spell):
         ans=0
+        exp = len(spell) - 1
         for ch in spell:
-            ans*=26
-            ans+=(ord(ch)-ord("a")+1)
+            ans += (pow(26, exp) * (ord(ch)-ord("a")+1))
+            exp -= 1
         return ans
+    
     def num2spell(num):
         rt=[]
         while num:
@@ -12,10 +14,13 @@ def solution(n, bans):
             num//=26
             num -= (rt[-1]==26)
         return "".join(map(lambda x:chr(x+ord("a")-1), rt[::-1]))
+    
     b=sorted([spell2num(spell) for spell in bans])
+    
     for od in b:
         if od<=n:
             n+=1
         else:
             break
+            
     return num2spell(n)
