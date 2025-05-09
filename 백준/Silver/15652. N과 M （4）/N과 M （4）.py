ced88f280e359924a271f179e4ee0bbb72636143
sujinjeongg <102423490+sujinjeongg@users.noreply.github.com>
2025-05-09 15:29:26 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,32 @@
+N, M = map(int, input().split())
+
+def solution(N, M):
+    output = []
+
+    def dfs(start, comb):
+        if len(comb) == M:
+            output.append(comb[:])
+            return 
+        
+        # 중복 조합 생성 
+        for i in range(start, N+1):
+            comb.append(i)
+            dfs(i, comb)
+            comb.pop()
+
+    dfs(1, [])
+
+    for comb in output:
+        print(" ".join(map(str, comb)))
+
+solution(N, M)
+
+'''
+def solution(N, M):
+    from itertools import combinations_with_replacement
+
+    for comb in combinations_with_replacement(range(1, N+1), M):
+        print(" ".join(map(str, comb)))
+
+solution(N, M) 
+''' 
