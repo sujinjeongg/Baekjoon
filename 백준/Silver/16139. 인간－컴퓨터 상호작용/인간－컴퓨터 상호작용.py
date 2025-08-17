3477ca8457691dc305a8d61c8e40708f30b25003
sujinjeongg <102423490+sujinjeongg@users.noreply.github.com>
2025-08-17 15:40:14 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 772 ms, Memory: 106404 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,13 +1,22 @@
-S = input()
+import sys
+input = sys.stdin.readline
+
+S = input().strip()
 q = int(input())
 
 questions = []
 for _ in range(q):
     a, l, r = input().split()
-    questions.append((a, int(l), int(r)))
+    questions.append((ord(a)-97, int(l), int(r)))
 
 def solution(S, q):
-    for a, l, r in questions:
-        print(S[l:r+1].count(a))
+    prefix = [[0] * (len(S)+1) for _ in range(26)]
+    for i in range(1, len(S) + 1):
+        for j in range(26):
+            prefix[j][i] = prefix[j][i-1]
+        prefix[ord(S[i-1]) - 97][i] += 1
         
-solution(S, q)        
+    for a, l, r in questions:
+        print(prefix[a][r+1] - prefix[a][l])
+
+solution(S, q)
